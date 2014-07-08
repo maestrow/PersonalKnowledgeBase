8ab92e20f1e411600cccb3ae559b152b834a345d
Andrey Shmelev <andrew.shmelev@gmail.com>
2014-07-08 13:28:56 +0400

    Инициализация БД

--- Blog/Global.asax.cs
+++ Blog/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -18,6 +19,7 @@ namespace Blog
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer(DbInitializer.Instance);
         }
     }
 }
